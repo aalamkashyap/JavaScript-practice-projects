eb329c2f177fdca6a3960a6022d5a56fcd982f94
Aalamdeep Kashyap <aalamkashyap36@gmail.com>
2024-05-08 18:39:31 +0530

    Add files via upload

---
+++ Forkify/src/js/config.js
@@ -0,0 +1,10 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SECONDS = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '5d46c581-e9d6-4b59-8520-e79692b840f0';
+export const MODAL_CLOSE_SEC = 2.5;
+
+// Display number of pages between pagination button
+// ability to sort search resukts by duration or number of ingredients - to be added in api
+// perform ingredient validation in view, before submitting form
+//improve recipe ingredient input - separate in multiple fields and allow more than 6 ingredients
